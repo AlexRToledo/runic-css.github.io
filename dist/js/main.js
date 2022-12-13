99fa047d3b50f7dcf4413045cd054f7d47bbbb34
Alejandro Rodriguez Toledo <artoledo91@gmail.com>
2022-12-12 23:33:34 -0600

    [FIX] Submit dist.

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+console.log('Start web-server')
+
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoianMvbWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUE7Ozs7Ozs7QUNBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3J1bmljLWNzcy8uL3NyYy9lbnRyaWVzL2luZGV4LmpzIiwid2VicGFjazovL3J1bmljLWNzcy8uL3NyYy9zYXNzL21haW4uc2NzcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnU3RhcnQgd2ViLXNlcnZlcicpXHJcbiIsIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
